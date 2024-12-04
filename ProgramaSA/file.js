9bffcc2b73e796c6f60b9bee1fe9dd406b404ce2
SteinShop <shopstein85@gmail.com>
2024-12-03 22:21:20 -0300

    Add files via upload

---
+++ ProgramaSA/file.js
@@ -0,0 +1,26 @@
+function MostraMensagem(){
+
+    if(document.getElementById('cadastro').style.display == 'none'){
+
+        document.getElementById('cadastro').style.display = 'block'
+
+    }else{
+
+        document.getElementById('cadastro').style.display = 'none'
+
+    }
+
+}
+
+function editar(){
+
+    
+    document.getElementById('cadastro').style.display = 'block';
+    document.getElementById('editar').style.display = 'none;
+    
+
+    
+
+
+
+}
